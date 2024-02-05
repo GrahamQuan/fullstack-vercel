91e19d38b8a9beda9d1965a79d200eb519251c5d
GrahamQuan <grahamquan24@gmail.com>
2024-02-06 02:40:50 +0800

    add cors

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello');
